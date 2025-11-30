e81835e08aa13f939458873558606353715180c7
Richard Binder <binder.richard@outlook.de>
2025-11-30 19:50:17 +0100

    Add scripts and gitignore

---
+++ scripts_new/compute_encoding_new.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ..
+
+# python -m src.LPCA.lpca_with_sim_runner_new ZINC 4 8 0.0 1000
+# python -m src.LPCA.lpca_with_sim_runner_new Peptides 4 8 0.0 50
+python -m src.LPCA.lpca_with_sim_runner_new CIFAR 4 12 2.0 50
